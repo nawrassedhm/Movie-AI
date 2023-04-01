fff8994d0f9be400d7a0594c9650a7b0539fadc5
Nawrasse Dahman <nawrassedhm@techie.com>
2023-04-01 11:30:53 +0000

    Update Footer.svelte

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -2,8 +2,6 @@
 	class="max-w-4xl mx-auto w-full flex md:flex-row flex-col items-center md:justify-between py-8 mt-20 border-t border-white/20"
 >
 	<div class="text-white/70 flex items-center flex-none">
-		Powered by <span class="mx-1 font-bold"> OpenAI</span>
-		and
-		<span class="ml-1 font-bold">Vercel Edge Functions</span>.
+		Powered by OpenAI And Vercel Edge Functions
 	</div>
 </div>
